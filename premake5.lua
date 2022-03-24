1f00606b32d9d05d66949b27033fd865b931957a
Robbe Bryssinck <robbebryssinck@gmail.com>
2022-03-24 19:48:36 +0100

    premake file

---
+++ premake5.lua
@@ -0,0 +1,18 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
